07dff882cd33342249e6408ad87d3ccb16851549
rptec <108799559+rptecs@users.noreply.github.com>
2025-03-31 13:29:14 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,8 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
       <span>
-        <span>NewsNow © 2024 By </span>
-        <a href={Author.url} target="_blank">
-          {Author.name}
-        </a>
+        <span>新闻聚合 © 2024-2025  </span>
       </span>
     </>
   )
